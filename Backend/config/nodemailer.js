3a1f5c7ef2fd6b6a18b820ab062f4597e212c3d4
Nabil201-ctrl <abubakar.nabil.210@gmail.com>
2025-10-06 13:43:42 +0100

    updates

--- Backend/config/nodemailer.js
+++ Backend/config/nodemailer.js
@@ -1,41 +1,18 @@
 const nodemailer = require('nodemailer');
-const { google } = require('googleapis');
-
-const OAuth2 = google.auth.OAuth2;
+const dotenv = require('dotenv')
+dotenv.config();
 
 const createTransporter = async () => {
-    const oauth2Client = new OAuth2(
-        process.env.CLIENT_ID,
-        process.env.CLIENT_SECRET,
-        'https://developers.google.com/oauthplayground'
-    );
+  // Create a transporter using Gmail + App Password
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.APP_PASSWORD,
+    },
+  });
 
-    oauth2Client.setCredentials({
-        refresh_token: process.env.REFRESH_TOKEN
-    });
-
-    const accessToken = await new Promise((resolve, reject) => {
-        oauth2Client.getAccessToken((err, token) => {
-            if (err) {
-                reject('Failed to create access token :(');
-            }
-            resolve(token);
-        });
-    });
-
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            type: 'OAuth2',
-            user: process.env.EMAIL,
-            accessToken,
-            clientId: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            refreshToken: process.env.REFRESH_TOKEN
-        }
-    });
-
-    return transporter;
+  return transporter;
 };
 
 module.exports = createTransporter;
